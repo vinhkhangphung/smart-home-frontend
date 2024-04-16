da1357c2f7c2fc2898d3fec61949d39051c2afe1
Khan <vikhang611@gmail.com>
2024-04-16 13:24:09 +0700

    create basic login page

--- src/routes/home/HomeRouter.jsx
+++ src/routes/home/HomeRouter.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import AboutPage from "../../pages/AboutPage";
 import HomePage from "../../pages/HomePage";
+import LandingPage from "../../pages/LandingPage";
 
 export default function HomeRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="about" element={<AboutPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/login" element={<LandingPage/>} />
     </Routes>
   );
 }
