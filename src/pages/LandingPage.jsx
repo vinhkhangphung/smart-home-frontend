e2e9a04cd090a0c7c28988b75ca8000b147212ba
Khan <vikhang611@gmail.com>
2024-04-16 13:35:16 +0700

    Add css folder to pages folder
    Add css folder and edit landing page css

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import './css/LandingPage.Module.css'
 
 const Login = (props) => {
   const [email, setEmail] = useState('')
